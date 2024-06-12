bcb163fa95ff8a742fe4ae1e09b73d7e7feee01b
gustavocardosorod <gustavocardoso1705@gmail.com>
2024-06-12 20:30:47 -0300

    comparar o fluxo

--- src/ControleDeCandidatos/ProcessoSeletivo.java
+++ src/ControleDeCandidatos/ProcessoSeletivo.java
@@ -4,6 +4,9 @@ public class ProcessoSeletivo {
 
     public static void main(String[] args) {
         System.out.println("Processo seletivo");
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
     }
 
     static void analisarCandidato(double salarioPretendido) {
@@ -11,7 +14,7 @@ public class ProcessoSeletivo {
         if (salarioBase > salarioPretendido) {
             System.out.println("LIGAR PARA O CANDIDATO");
         } else if (salarioBase == salarioPretendido)
-            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
         else {
             System.out.println("AGUARDANDO DEMAIS CANDIDATOS");
         }
