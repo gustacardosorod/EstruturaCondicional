5ade2d1b4119b1d024119cf77810f9b11c8b63d5
gustavocardosorod <gustavocardoso1705@gmail.com>
2024-06-12 21:02:30 -0300

    seleção de candidatos

--- src/ControleDeCandidatos/ProcessoSeletivo.java
+++ src/ControleDeCandidatos/ProcessoSeletivo.java
@@ -1,12 +1,36 @@
 package ControleDeCandidatos;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-        System.out.println("Processo seletivo");
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        // Array com a lista de candidatos
+
+        String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO", "MÔNICA", "FABRÍCIO", "MIRELA",
+                "DANIELA", "JORGE" };
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println(" O Candidato" + candidato + "Solocicitou este valor de salario" + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O Candidato" + candidato + "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido) {
